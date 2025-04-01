4e28909dc5602214445805d1f593fbb271ea16fc
jenfire23 <jendrik@fortmeier.biz>
2025-04-01 18:58:17 +0200

    010425

--- sub/web.js
+++ sub/web.js
@@ -14,11 +14,27 @@ fetch('values.json')
     createChart(data, type)
 })
 
+
+
+addData()
+
+function addData(chart, label, newData) {
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+      dataset.data.push(newData);
+  });
+  chart.update();
+}
+
+
+
+
+
 function createChart(data, type){
-new Chart(ctx, {
+new Chart1(ctx, {
   type: type,
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: Object.keys(data),
     datasets: [{
       label: '# of Votes',
       data: [12, 19, 3, 5, 2, 3],
