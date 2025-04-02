f33ed7372ceaa52fb58843a7047c2c03b5eccdef
jenfire23 <jendrik@fortmeier.biz>
2025-04-02 11:07:12 +0200

    json test

--- sub/web.js
+++ sub/web.js
@@ -11,42 +11,11 @@ fetch('values.json')
 
 .then(function(data){
     console.log(data)
-    createChart(data, type)
 })
 
-
-
-addData()
-
-function addData(chart, label, newData) {
-  chart.data.labels.push(label);
-  chart.data.datasets.forEach((dataset) => {
-      dataset.data.push(newData);
-  });
-  chart.update();
-}
-
-
-
-
-
-function createChart(data, type){
-new Chart(ctx, {
-  type: type,
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
+for (const col in data) {
+  console.log(`${col}:`)
+  for (const row in col) {
+    console.log(`${row}:`)
   }
-});
 }
