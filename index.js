de67669ba27da671aa4005a7e5c363b699a34933
jenfire23 <jendrik@fortmeier.biz>
2025-04-07 11:22:54 +0200

    times changes

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ fetch('sub/info.json')
                 var time_u = data["update_time"]
                 var time_d = data["data_time"]
                 console.log(time_u, time_d)
-                document.getElementById("txt-u").innerHTML = JSON.stringify(time_u)
-                document.getElementById("txt-d").innerHTML = JSON.stringify(time_d)
+
+                var txt_u = document.createElement("div");
+                txt_u.appendChild(document.createTextNode(time_u));
+                txt_u.id = "time-u"
+                var txt_d = document.createElement("div");
+                txt_d.appendChild(document.createTextNode(time_d));
+                txt_d.id = "time-d"
+                var time_s = document.getElementById("times");
+                time_s.appendChild(txt_u);
+                time_s.appendChild(txt_d);
             })
