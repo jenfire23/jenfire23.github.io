278ad6566204307eb0ad2a0781aae1b8b549006f
jenfire23 <jendrik@fortmeier.biz>
2025-04-07 10:40:59 +0200

    text update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 fetch('sub/info.json')
             .then(response => response.json()) // Parse JSON
-            .then(data => function(data) {
+            .then(function(data) {
                 console.log(data) // Work with JSON data
                 var time_u = data["update_time"]
                 var time_d = data["data_time"]
                 console.log(time_u, time_d)
-                document.getElementById("time-u").innerHTML = time_u
-                document.getElementById("time-d").innerHTML = time_d
+                document.getElementById("time-u").textContent(txt_u)
+                document.getElementById("time-d").textContent(txt_d)
             })
             .catch(error => console.error('Error fetching JSON:', error));
             
