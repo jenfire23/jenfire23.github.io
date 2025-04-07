869c92fc284515c3fa546cb2e6629407f7aa9466
jenfire23 <jendrik@fortmeier.biz>
2025-04-07 10:25:56 +0200

    text changes

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ fetch('sub/info.json')
                 var time_u = data["update_time"]
                 var time_d = data["data_time"]
                 console.log(time_u, time_d)
+                document.getElementById("time-u").innerHTML = time_u
+                document.getElementById("time-d").innerHTML = time_d
             })
             .catch(error => console.error('Error fetching JSON:', error));
             
 
-        document.getElementById("time-u").innerHTML = time_u
-        document.getElementById("time-d").innerHTML = time_d
+        
