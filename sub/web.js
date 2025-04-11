4bbdae9e5ca6bdb47ff2d1284a33f9270983e7dd
jenfire23 <jendrik@fortmeier.biz>
2025-04-11 10:04:46 +0200

    publish 11.04.25 - 10:04:46

--- sub/web.js
+++ sub/web.js
@@ -1,9 +1,9 @@
-const ctx = document.getElementById('myChart');
-var type = 'doughnut'
-var webchart
+const ctx = document.getElementById('myChart');                           // Chart element (canvas)
+var type = 'doughnut'                                                     // Diagram type
+var webchart                                                              // Chart variable
+var i_typ = 0
 
-
-fetch('values.json')
+fetch('values.json')// Get values from json
 .then(function(response){
     if (response.ok == true) {
         return response.json()
@@ -12,16 +12,16 @@ fetch('values.json')
 
 .then(function(data){
     console.log(data)
-    json_out(data, 1)
+    json_out(data, 1)// Run program
 })
 
-fetch('info.json')
-    .then(response => response.json()) // Parse JSON
-    .then(data => console.log(data)) // Work with JSON data
-    .catch(error => console.error('Error fetching JSON:', error));
+//fetch('info.json')// Get website info
+//    .then(response => response.json()) // Parse JSON
+//    .then(data => console.log(data)) // Work with JSON data
+//    .catch(error => console.error('Error fetching JSON:', error));
 
-function time_graph() {
-  fetch('detail.json')
+function time_graph() {// Time graph - does not exist yet
+  fetch('detail.json')// Get info for time graph
   .then(function(response){
       if (response.ok == true) {
           return response.json()
@@ -34,7 +34,7 @@ function time_graph() {
   })
 }
 
-function graph_out(data) {
+function graph_out(data) {// Building time graph
   var dat = {}
   for (var tp of Object.keys(data)) {
     for (var tp2 of Object.keys(data[tp])) {
@@ -56,18 +56,18 @@ function graph_out(data) {
 }
 
 
-function json_out(data, btn_col, bt) {
+function json_out(data, btn_col, bt) { // Building normal graph
   var ch_chart = []
   var d_labels = {}
   var d_data = {}
-  for (var el of Object.keys(data)) { // json
+  for (var el of Object.keys(data)) { // In values list, every column
     console.log(el)
     var ch_labels = []
     var ch_data = []
-    for (var col of Object.keys(data[el])) { // column
+    for (var col of Object.keys(data[el])) { // Current column
       console.log(`${col} \----------`);
       if (bt != 1) {new_btn(data, col)}
-      for (var row of Object.keys(data[el][col])) { // row
+      for (var row of Object.keys(data[el][col])) { // Rows in column
         //console.log(`${row}: ${data[el][col][row]}`);
         var ch_l = row
         var ch_d = data[el][col][row]
@@ -80,7 +80,7 @@ function json_out(data, btn_col, bt) {
       d_labels[col] = ch_labels
       d_data[col] = ch_data
     }
-    console.log(ch_labels)
+    console.log(ch_labels)// Console logs fpr testing
     console.log(ch_data)
     console.log(ch_chart)
     console.log(d_labels)
@@ -93,25 +93,43 @@ function json_out(data, btn_col, bt) {
 
     console.log(btn_col)
 
-    //if (cr == 1) {
-      createChart(chart_d, chart_l, btn_col, type) // Value - Label - Column - ChartType
-    //  cr = 0
-    //}
+    createChart(chart_d, chart_l, btn_col, change_typ()) // Create Chart with the parameters
+               // Value - Label - Column - ChartType
   }
   time_graph()
 }
 
 
-function new_btn(data, col) {
-  var element = document.createElement("button");
-  element.appendChild(document.createTextNode(col));
-  element.id = `votebtn-${col}`
-  element.addEventListener('click', function () {json_out(data, col, 1)})
-  var page = document.getElementById("votee");
-  page.appendChild(element);
+function new_btn(data, col) { // Create buttons above (for column change) | data for json_out | col as name
+  var element = document.createElement("button");// create button
+  element.appendChild(document.createTextNode(col));// col from json_out as text
+  element.id = `votebtn-${col}`// create unique button id
+  element.addEventListener('click', function () {json_out(data, col, 1)}) // add click function
+  var page = document.getElementById("votee"); // find button container
+  page.appendChild(element); // add button to container
 }
 
-function createChart(ch_data, ch_labels, ch_type, type){
+function change_typ() {
+  var typs = ["bar", "pie", "line"]
+  console.log(i_typ)
+  console.log(type)
+  if (i_typ != 2) {
+    i_typ += 1
+    console.log(i_typ)
+  }
+  else {i_typ = 0
+    console.log(i_typ)
+  }
+  type = typs[i_typ]
+  console.log(i_typ)
+  console.log(type)
+  return type
+}
+
+
+
+
+function createChart(ch_data, ch_labels, ch_type, type){// chart function, using chart.js (reference link in start.html)
   if (webchart) {
     webchart.destroy()
   }
